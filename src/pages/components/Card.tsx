b4dbb7c77df0f69efa260c5719996536ace6000b
greypeg <gpapas028@gmail.com>
2022-12-21 17:23:11 +0000

    first commit created card component

---
+++ src/pages/components/Card.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Link from "next/link";
+export interface ICardProps {
+    title: string;
+    message: string;
+}
+
+export function Card (props: ICardProps) {
+    const {title, message} = props
+  return (
+    <div
+    className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+  >
+    <h3 className="text-2xl font-bold">{title}</h3>
+    <div className="text-lg">
+      {message}
+    </div>
+  </div>
+  );
+}
